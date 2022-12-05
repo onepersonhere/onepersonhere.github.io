dc654d826988fb94bc1593a53236bf699d153560
onepersonhere <51043502+onepersonhere@users.noreply.github.com>
2022-12-05 15:31:31 +0800

    Website updates.

--- assets/js/bs-init.js
+++ assets/js/bs-init.js
@@ -93,4 +93,9 @@ document.addEventListener('DOMContentLoaded', function() {
 	}
 
 })();
+
+	var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bss-tooltip]'));
+	var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+	  return new bootstrap.Tooltip(tooltipTriggerEl);
+	})
 }, false);
